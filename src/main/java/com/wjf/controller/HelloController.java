8eeb3390b1441f938adba4086f8039488d3e359c
weijunfeng <weijunfeng@jd.com>
2018-11-14 10:23:22 +0800

    java -jar spring-boot-03-1.0.0-SNAPSHOT.jar --server.port=8999 --spring.profiles.active=prd
    命令行运行jar文件指定端口号，指定运行的环境

--- src/main/java/com/wjf/controller/HelloController.java
+++ src/main/java/com/wjf/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.wjf.controller;
 
+import com.wjf.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private User user;
     @RequestMapping(value = "hello")
     public String hello(){
-        return "hello,world";
+        return "hello,world"+user.getPwd();
     }
 }
---
+++ src/main/java/com/wjf/entity/User.java
@@ -0,0 +1,39 @@
+package com.wjf.entity;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "user")
+public class User {
+    private Integer id;
+    private String name;
+    private String pwd;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
